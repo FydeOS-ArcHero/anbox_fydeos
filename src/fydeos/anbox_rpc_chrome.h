64095d04fb296217238fdacb58c263eb3bf3c80b
tean <this.is.tean@gmail.com>
2020-12-05 08:56:51 +0000

    changed socket path

--- src/fydeos/anbox_rpc_chrome.h
+++ src/fydeos/anbox_rpc_chrome.h
@@ -1,6 +1,6 @@
 #ifndef ANBOX_RPC_CHROME_H_
 #define ANBOX_RPC_CHROME_H_
 
-#define ANBOX_RPC_CHROME_NAME   "/run/chrome/anbox_rpc_chrome"
+#define ANBOX_RPC_CHROME_NAME   "/run/chrome/anbox/sockets/anbox_chrome"
 
 #endif
